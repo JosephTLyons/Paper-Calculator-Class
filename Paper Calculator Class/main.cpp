be956a4e84648aa02c950a5e96b58c5485e023ab
Joseph Lyons <josephlyons@josephs-air.pk5001z>
2017-02-27 23:43:12 -0500

    Initial Commit

---
+++ Paper Calculator Class/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Paper Calculator Class
+//
+//  Created by Joseph Lyons on 2/27/17.
+//  Copyright © 2017 Joseph Lyons. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
